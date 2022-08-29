603f9a39de063f2c138189db76e56989746d2270
juan baez <juabaez@gmail.com>
2022-08-29 17:43:40 -0300

    implement map service. closes #6

---
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package juan.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import juan.springframework.sfgpetclinic.model.Pet;
+import juan.springframework.sfgpetclinic.services.CrudService;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+  @Override
+  public Set<Pet> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public void delete(Pet object) {
+    super.delete(object);
+  }
+
+  @Override
+  public Pet save(Pet object) {
+    return super.save(object.getId(), object);
+  }
+
+  @Override
+  public Pet findById(Long id) {
+    return super.findById(id);
+  }
+}
