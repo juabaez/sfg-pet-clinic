4b477ee0762fedb57c593c90894b24faf4ea9201
juan baez <juabaez@gmail.com>
2022-09-01 18:52:07 -0300

    List vets. closes #15

--- pet-clinic-web/src/main/java/juan/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/juan/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package juan.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import juan.springframework.sfgpetclinic.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
-  @RequestMapping({"","/","/index","/index.html"})
-  public String listVets(){
+  private final VetService vetService;
+
+  public VetController(VetService vetService) {
+    this.vetService = vetService;
+  }
+
+  @RequestMapping({ "", "/", "/index", "/index.html" })
+  public String listVets(Model model) {
+    model.addAttribute("vets", vetService.findAll());
+
     return "vets/index";
   }
 }
