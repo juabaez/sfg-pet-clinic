830f0b1b1afb36ceafc6c83d99e807e742b11612
juan baez <juabaez@gmail.com>
2022-09-21 15:58:41 -0300

    add pettype service impl. closes #29

---
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package juan.springframework.sfgpetclinic.services;
+
+import juan.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+
+}
