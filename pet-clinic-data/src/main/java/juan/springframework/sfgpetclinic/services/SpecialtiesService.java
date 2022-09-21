1cad89085bb0122a0b0a2eb020c77ae33fb79e14
juan baez <juabaez@gmail.com>
2022-09-21 16:38:30 -0300

    add speciality service impl. closes #31

---
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package juan.springframework.sfgpetclinic.services;
+
+import juan.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality,Long>{
+
+}
---
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,36 @@
+package juan.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import juan.springframework.sfgpetclinic.model.Speciality;
+import juan.springframework.sfgpetclinic.services.SpecialtiesService;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+  @Override
+  public Set<Speciality> findAll() {
+    return super.findAll();
+  }
+
+  @Override
+  public void deleteById(Long id) {
+    super.deleteById(id);
+  }
+
+  @Override
+  public Speciality save(Speciality object) {
+    return super.save(object);
+  }
+
+  @Override
+  public void delete(Speciality object) {
+    super.delete(object);
+  }
+
+  @Override
+  public Speciality findById(Long id) {
+    return null;
+  }
+}
