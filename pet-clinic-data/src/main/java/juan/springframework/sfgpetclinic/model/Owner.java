5e52e82a733262e37ec17c9595018106184e2e83
juan baez <juabaez@gmail.com>
2022-10-27 12:24:36 -0300

    conver to JPA closes #36

--- pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Owner.java
@@ -3,11 +3,26 @@ package juan.springframework.sfgpetclinic.model;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+  @Column
   private String address;
+
+  @Column
   private String city;
+
+  @Column
   private String telephone;
+
+  @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
   private Set<Pet> pets = new HashSet<>();
 
   public String getAddress() {
