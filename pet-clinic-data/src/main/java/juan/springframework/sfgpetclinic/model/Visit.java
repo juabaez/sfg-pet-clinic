1dd2fa2c8b1a82b1376e74e5e41ccc29b1836aba
juan baez <juabaez@gmail.com>
2022-10-27 12:44:02 -0300

    convert to JPA closes #38

--- pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Visit.java
@@ -2,10 +2,24 @@ package juan.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
 
+  @Column
   private LocalDate date;
+
+  @Column
   private String description;
+
+  @ManyToOne
+  @JoinColumn(name = "pet_id")
   private Pet pet;
 
   public LocalDate getDate() {
