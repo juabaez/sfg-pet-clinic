6fd367efd3b2aa46e72ff965cf824ffb20629bbf
juan baez <juabaez@gmail.com>
2022-10-27 12:34:00 -0300

    convert to JPA closes #37

--- pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/juan/springframework/sfgpetclinic/model/Speciality.java
@@ -1,6 +1,15 @@
 package juan.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Table;
+
+@MappedSuperclass
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
+
+  @Column
   private String description;
 
   public String getDescription() {
