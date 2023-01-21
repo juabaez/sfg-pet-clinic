e3d3f01793a271c4538c8058fb39d3aa29635d02
juan baez <juabaez@gmail.com>
2023-01-21 17:42:55 -0300

    data binder

--- pet-clinic-web/src/main/java/juan/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/juan/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -2,7 +2,9 @@ package juan.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -21,9 +23,13 @@ public class OwnerController {
     this.ownerService = ownerService;
   }
 
+  @InitBinder
+  public void setAllowFields(WebDataBinder dataBinder) {
+    dataBinder.setDisallowedFields("id");
+  }
+
   @RequestMapping({ "", "/", "/index", "/index.html" })
   public String listOwners(Model model) {
-
     model.addAttribute("owners", ownerService.findAll());
     System.out.println("Owners size " + ownerService.findAll().size());
     return "owners/index";
